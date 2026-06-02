17b3b68abbd834593f948923be1fe298cb2c30de
LuizFNeto20 <luizfneto201120@gmail.com>
2026-06-01 21:46:52 -0400

    refactor: rename sfr-orquestrador-api to sfr-orchestrator-api

--- sfr-orchestrator-api/src/main/java/com/sfr/sfr_orquestrador_api/SfrOrchestratorApiApplication.java
+++ sfr-orchestrator-api/src/main/java/com/sfr/sfr_orquestrador_api/SfrOrchestratorApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SfrOrquestradorApiApplication {
+public class SfrOrchestratorApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfrOrquestradorApiApplication.class, args);
+		SpringApplication.run(SfrOrchestratorApiApplication.class, args);
 	}
 
 }
